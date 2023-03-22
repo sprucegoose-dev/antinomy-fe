07e9ab08dd53c8f5b973c920df90f804186c7360
Morgan Polak <polakmaor@gmail.com>
2023-03-22 23:34:04 +0000

    ENH: updates the 'Rules' section

--- src/components/Rules/Rules.tsx
+++ src/components/Rules/Rules.tsx
@@ -27,6 +27,13 @@ export function Rules(): JSX.Element {
                     <li>
                         Deal 1 Sorcerer card to each player.
                     </li>
+                    <li>
+                        Choose a starting player at random.
+                    </li>
+                    <li>
+                        In turn order, each player places their Sorcerer card below a card on the
+                         Continuum that matches the Codex color.
+                    </li>
                 </ul>
             </div>
             <div className="subtitle">
@@ -35,20 +42,18 @@ export function Rules(): JSX.Element {
             <div className="content">
                 <ul>
                     <li>
-                        Choose a starting player at random.
-                    </li>
-                    <li>
                         Select a card from your hand.
                     </li>
                     <li>
-                        Use the <b>value</b> (number) of the card to move your Sorcerer to the <b>right</b> (forward in time).
+                        Use the <b>value</b> (number) of the card to move your Sorcerer that many
+                         spaces to the <b>right</b> (forward in time).
                     </li>
                     <li>
-                        Use the <b>color</b> or <b>suit</b> (symbol) of your card to move your Sorcerer to
-                         the <b>left</b> to any card card with a matching <b>color</b>/<b>suit</b> (backward in time).
+                        Use the <b>color</b> or <b>suit</b> (symbol) of your card to move your Sorcerer to any
+                         card on the <b>left</b> with a matching <b>color</b>/<b>suit</b> (backward in time).
                     </li>
                     <li>
-                        Swap the card from your hand with the card from the Contiuum at your Sorcrer's final position.
+                        Swap the card from your hand with the card from the Contiuum at your Sorcerer's final position.
                     </li>
                 </ul>
             </div>
@@ -61,11 +66,11 @@ export function Rules(): JSX.Element {
                         After collecting a card from the Continuum, check to see if you have a matching set.
                     </li>
                     <li>
-                        In order to have a matching set, all three cards in your hand must have the same <b>number</b>, <b>color</b>, or <b>suit</b>,
-                        &nbsp;<i>and none of them belong to the Codex color.</i>
+                        In order to have a matching set, all three cards in your hand must have the same <b>number</b>, <b>color</b>, or <b>suit</b>
+                        , <i>and none of them belong to the Codex color.</i>
                     </li>
                     <li>
-                        If you have a matching set (called a Paradox), gain <b>1 point</b>, <b>advance the Codex color clockwise</b> to the next color,
+                        If you have a valid matching set (called a Paradox), gain <b>1 point</b>, <b>advance the Codex color clockwise</b> to the next color,
                         and <b>swap your cards with 3 cards from the Continuum</b> to the left or right (if possible).
                     </li>
                 </ul>
@@ -79,7 +84,8 @@ export function Rules(): JSX.Element {
                         If your Sorcerer ends its movement on the same card as the other Sorcerer, combat begins.
                     </li>
                     <li>
-                       Players compare the cards in their hand, and the player with the highest total value wins.
+                       Players compare the cards in their hand, and the player with the highest total value
+                        wins. <i>Cards in the active Codex color are ignored.</i>
                     </li>
                     <li>
                         In case of a tie, players shuffle their cards, and randomly reveal one card at a time. The card with the
